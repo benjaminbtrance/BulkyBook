4d48d58da3d898bc59fe7e98e70722deae77d091
benjaminbtrance <benjaminb.tran@gmail.com>
2022-06-17 22:39:27 -0500

    Category Crud, Create Category Controller, Category Index Get All Actions, Category Index View, Category JS, Category Upset Get Action, Partial View, Category Upset View, Category Upset Post Action, Category Delete API Call, Category Delete JS

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BulkyBook.Models;
 
 namespace BulkyBook.DataAccess.Data
 {
@@ -12,5 +13,7 @@ namespace BulkyBook.DataAccess.Data
             : base(options)
         {
         }
+        
+        public DbSet<Category> Categories { get; set; }
     }
 }
